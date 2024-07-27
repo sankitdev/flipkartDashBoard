025d63fd20959f726df4980d3c32e158bad12bcd
Ankit Singh <ankitsingh635101@gmail.com>
2024-07-27 20:26:26 +0530

    Changed Icons source

--- FrontEnd/src/Components/Icon.jsx
+++ FrontEnd/src/Components/Icon.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import { CgAddR } from "react-icons/cg";
+import { FaBell } from "react-icons/fa";
+import { CiChat1 } from "react-icons/ci";
+import { FaRegQuestionCircle } from "react-icons/fa";
 import MyDropzone from "./MyDropzone";
 const Icon = () => {
   const [visible, setVisible] = useState(false);
@@ -8,10 +12,10 @@ const Icon = () => {
   return (
     <>
       <div className="text-2xl gap-8 flex absolute right-10">
-        <i className="ri-file-add-line mr-20" onClick={handleClick}></i>
-        <i className="ri-notification-line"></i>
-        <i className="ri-chat-1-line"></i>
-        <i className="ri-questionnaire-line"></i>
+        <CgAddR onClick={handleClick} />
+        <FaBell />
+        <CiChat1 />
+        <FaRegQuestionCircle />
       </div>
       {visible && <MyDropzone setVisible={setVisible} />}
     </>
